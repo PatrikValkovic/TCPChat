961866e6509ec62e83ee7b28455956145c743138
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-05-24 14:18:54 +0200

    Added creating of client in index.js

--- server/index.js
+++ server/index.js
@@ -10,15 +10,23 @@ const net = require('net')
 const Log = require('../shared/logger')
 const config = require('../config.json')
 const GroupManager = require('./GroupManager')
+const Client = require('./Client')
 
 if(require.main === module)
 {
+    let counter = 10
     const grpManager = new GroupManager(config.defaultGroups)
-
     const server = net.createServer()
 
     server.on('connection',(socket) => {
-
+        const client = new Client(socket,counter++)
+        socket.on('end',()=>{
+            client.disconnect()
+        })
+        socket.on('data',(cont)=>{
+            const req = JSON.parse(cont.toString())
+            console.log(cont)
+        })
     })
 
     server.listen(config.port,config.host,()=>{

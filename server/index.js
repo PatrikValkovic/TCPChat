c401e11125b93dd626ffbce54e597d534b8ecceb
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-05-24 13:21:18 +0200

    Added server creation

--- server/index.js
+++ server/index.js
@@ -5,3 +5,21 @@
  * Created by patri
  */
 'use strict'
+
+const net = require('net')
+const Log = require('../shared/logger')
+const config = require('../config.json')
+
+if(require.main === module)
+{
+    const server = net.createServer()
+
+    server.on('connection',(socket) => {
+
+    })
+
+    server.listen(config.port,config.host,()=>{
+        const address = server.address()
+        Log.info(`Server running on ${address.address}:${address.port}`)
+    })
+}

18bb477707ed1a16c6c58770b46e813ba1032384
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-05-24 13:59:43 +0200

    Created client class

---
+++ server/Client.js
@@ -0,0 +1,19 @@
+/**
+ * 24.05.2017
+ * TCP Chat using NodeJS
+ * https://github.com/PatrikValkovic/TCPChat
+ * Created by patri
+ */
+'use strict'
+
+module.exports = class Client{
+    constructor(socket,id){
+        this.id = id
+        this.socket = socket
+        this.name = 'anonymous'
+    }
+
+    disconnect(){
+
+    }
+}
